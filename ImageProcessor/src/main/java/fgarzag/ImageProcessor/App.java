7e2115688997363414e172846db1c159d40b737a
Fernando Garza <fernando.garza@oracle.com>
2019-12-20 11:58:26 -0600

    Maven project structure

---
+++ ImageProcessor/src/main/java/fgarzag/ImageProcessor/App.java
@@ -0,0 +1,13 @@
+package fgarzag.ImageProcessor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
