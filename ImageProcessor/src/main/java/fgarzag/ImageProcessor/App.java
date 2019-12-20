d49e3564db57bf82f54ce9df8cb471abfb59c645
Fernando Garza <fernando.garza@oracle.com>
2019-12-20 13:33:27 -0600

    Finished main application, no threads or patterns

--- ImageProcessor/src/main/java/fgarzag/ImageProcessor/App.java
+++ ImageProcessor/src/main/java/fgarzag/ImageProcessor/App.java
@@ -1,13 +1,44 @@
 package fgarzag.ImageProcessor;
 
-/**
- * Hello world!
- *
- */
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
+import javax.imageio.ImageIO;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+		
+        BufferedReader reader = null;
+		try {
+			reader = new BufferedReader(new FileReader("src/main/resources/URLs.txt"));
+		} catch (FileNotFoundException e1) {
+			System.out.println("URL file could not be found");
+			System.exit(0);
+		}
+        
+		String line;
+        try {
+			while((line = reader.readLine()) != null) {
+				System.out.println(line);
+				String image_name = line.substring(line.lastIndexOf('/')+1);
+				System.out.println(image_name);
+				
+				BufferedImage image = null;
+				URL url = new URL(line);
+				image = ImageIO.read(url);
+				
+				ImageIO.write(image, "jpg", new File("src/main/resources/Images/"+image_name));
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
     }
 }
